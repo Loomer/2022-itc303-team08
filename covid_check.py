324aaa6606c4beb25c65ee32055aaf113f778e08
manveersingh saluja <manusaluja0@gmail.com>
2022-06-03 23:26:02 +1000

    comments added to the code

--- covid_check.py
+++ covid_check.py
@@ -1,37 +1,36 @@
+#Importing necessary modules
 import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 import cv2
 
-"""def covid_check(link):
-  model = load_model("C:/Users/manve/Desktop/new_model.h5")
-  path = link
-  img = cv2.imread(path)
-  test_image = cv2.resize(img, (250,250))
-  test_image = np.expand_dims(test_image, axis=0)
-  images = np.vstack([test_image])
-  prediction = model.predict(images)
-  return np.argmax(prediction[0])"""
-
-interpreter = tf.lite.Interpreter("C:/Users/manve/Desktop/tflite_model_fp16.tflite")
+#Creating an interpreter for the tf lite file and allocating sensors
+interpreter = tf.lite.Interpreter("C:/Users/manve/Desktop/tflite_model_fp16.tflite") 
 interpreter.allocate_tensors()
 
-
+#Function to check lung type
 def covid_check(link):
-  img = cv2.imread(link)
-  test_image = cv2.resize(img, (250,250))
+  
+  #Processing the image to work with the model
+  img = cv2.imread(link) 
+  test_image = cv2.resize(img, (250,250)) 
   test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
+
+  #Setting requirements for the interpreter
   input_index = interpreter.get_input_details()[0]["index"]
   output_index = interpreter.get_output_details()[0]["index"]
   interpreter.set_tensor(input_index, test_image)
+
+  #Predicting the image type
   interpreter.invoke()
-  predictions = interpreter.get_tensor(output_index)
+  predictions = interpreter.get_tensor(output_index) 
   if (str(predictions)[2] == "0"):
     return "NORMAL"
   elif (str(predictions)[2] == "1"):
     return "COVID"
-  
+
+#Calling the function with the path
 temp = input("Please enter path")
 print(covid_check(temp))
 
