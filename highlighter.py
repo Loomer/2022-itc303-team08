3b98050ee8e82ec36c0ee035d8f625432eebac60
manveersingh saluja <manusaluja0@gmail.com>
2022-10-06 11:57:30 +1100

    process_image function added for the highlighting feature.

--- highlighter.py
+++ highlighter.py
@@ -6,51 +6,93 @@ import os
 
 def highlighter(img):
 
-  #Using the masked function on the image
-  masked(img)
+    #Using the masked function on the image
+    masked(img)
 
-  img1 = cv.imread("img1.png")
-  img2 = cv.imread("img2.png")
+    #Taking the two images to use
+    img1 = cv.imread("img1.png")
+    img2 = cv.imread("img2.png")
 
-  dst = cv.addWeighted(img1,1,img2,1,0)
+    #Processsing them to get a new image
+    dst = cv.addWeighted(img1,1,img2,1,0)
 
-  data = Image.fromarray(dst)
-  data.save('img3.png')
+    #Saving the image
+    data = Image.fromarray(dst)
+    data.save('img3.png')
+
+    process_image("img3.png")
 
 def masked(img):
 
-  #read the image
-  img = cv.imread(img)
+    #read the image
+    img = cv.imread(img)
 
-  #convert the BGR image to HSV colour space
-  hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    #convert the BGR image to HSV colour space
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-  #set the lower and upper bounds for the hue
-  lower = np.array([0,0,0])
-  upper = np.array([110,150,150])
+    #set the lower and upper bounds for the hue
+    lower = np.array([0,0,0])
+    upper = np.array([110,150,150])
 
-  #create a mask using inRange function
-  mask = cv.inRange(hsv, lower, upper)
+    #create a mask using inRange function
+    mask = cv.inRange(hsv, lower, upper)
 
-  #perform bitwise and on the original image arrays using the mask
-  res = cv.bitwise_not(mask)
+    #perform bitwise and on the original image arrays using the mask
+    res = cv.bitwise_not(mask)
 
-  #Saving the image
-  data = Image.fromarray(res)
-  data.save('img1.png')
+    #Saving the image
+    data = Image.fromarray(res)
+    data.save('img1.png')
 
-  #Doing the same thing again
-  lower = np.array([0,0,0])
-  upper = np.array([110,90,90])
+    #Doing the same thing again
+    lower = np.array([0,0,0])
+    upper = np.array([110,90,90])
 
-  #create a mask for green colour using inRange function
-  mask = cv.inRange(hsv, lower, upper)
+    #create a mask for green colour using inRange function
+    mask = cv.inRange(hsv, lower, upper)
 
-  #perform bitwise and on the original image arrays using the mask
-  res = cv.bitwise_not(mask)
+    #perform bitwise and on the original image arrays using the mask
+    res = cv.bitwise_not(mask)
 
-  #Saving the image
-  data = Image.fromarray(mask)
-  data.save('img2.png')
+    #Saving the image
+    data = Image.fromarray(mask)
+    data.save('img2.png')
+
+def process_image(image):
+    count = 0
+    total_count = 0
+    color1 = (0, 0, 0)
+    color2 = (255, 255, 255)
+    new_color = (255, 0, 0)
+    img = Image.open(image).convert('RGB')
+    w, h = img.size
+    print(w , h)
+    
+    for i in range(w):
+        for j in range((int)(h*0.20)):
+            current_color = img.getpixel( (i,j) )
+            if current_color == color2:
+                img.putpixel( (i,j), color1)
+
+    for i in range(w):
+        for j in range((int)(h*0.20), h):
+            current_color = img.getpixel( (i,j) )
+            if current_color == color2:
+                img.putpixel( (i,j), color1)
+            if current_color == color1:
+                img.putpixel( (i,j), new_color)
+
+    for i in range((int)(w*0.1)):
+        for j in range(h):
+            current_color = img.getpixel( (i,j) )
+            if current_color == new_color:
+                img.putpixel( (i,j), color1)
+
+    for i in range((int)(w*0.9), w):
+        for j in range(h):
+            current_color = img.getpixel( (i,j) )
+            if current_color == new_color:
+                img.putpixel( (i,j), color1)
+    img.save("img4.png")
 
 highlighter("C:/Users/manve/Desktop/ml-images/covid_image.png")
